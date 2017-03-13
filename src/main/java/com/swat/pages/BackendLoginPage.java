5d22ad9c0db11217e3a37fd1d35b8c74b97b63d1
Aleksey <aleksey.shevchuk@attractgroup.com>
2017-03-13 11:20:31 +0200

    This class was changed during created the 'AdminPanelAuthorizeTest' test

--- src/main/java/com/swat/pages/BackendLoginPage.java
+++ src/main/java/com/swat/pages/BackendLoginPage.java
@@ -1,17 +1,34 @@
 package com.swat.pages;
 
 import com.swat.BasePage;
+import com.swat.data.UserData;
 import com.swat.staticdata.PageTitle;
 import com.swat.staticdata.PageUrl;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * Created by paul on 07.03.17.
  */
 public class BackendLoginPage extends BasePage {
-    public BackendLoginPage(WebDriver driver, PageTitle title, PageUrl pageUrl) {
+	
+	@FindBy(xpath = "/html/body/div[2]/div/div/div/div[2]/form/div[1]/input")
+	private WebElement email;
+	@FindBy(xpath = "/html/body/div[2]/div/div/div/div[2]/form/div[2]/input")
+	private WebElement password;	
 
-            super(driver, PageTitle.BACKEND_LOGIN_PAGE, PageUrl.BACKEND_LOGIN_PAGE);
-        }
-    }
+	
+	public BackendLoginPage(WebDriver driver) {
+		super(driver, PageTitle.BACKEND_LOGIN_PAGE, PageUrl.BACKEND_LOGIN_PAGE);
+   	}
+	
+	//Set email and password in textbox and click on button
+	public AdminMainPage loginAs(UserData admin) {
+		getForm().set(email, admin.getName()).set(password, admin.getPassword()).submit();
+		return BasePage.create(driver, AdminMainPage.class);
+	}	
+	
+}
 
