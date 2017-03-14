c1fc598eac80d1c30d2e1ba1212394978e898c00
Aleksey <aleksey.shevchuk@attractgroup.com>
2017-03-14 11:59:10 +0200

    This class was created during created the 'AdminPanelAuthorizeTest' test

--- src/main/java/com/swat/pages/AdminMainPage.java
+++ src/main/java/com/swat/pages/AdminMainPage.java
@@ -3,6 +3,8 @@ package com.swat.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.swat.BasePage;
 import com.swat.data.UserData;
@@ -12,19 +14,66 @@ import com.swat.staticdata.PageUrl;
 public class AdminMainPage extends BasePage {
 
 	@FindBy(xpath = "/html/body/div[2]/div[2]/div/div[1]/div[2]/div/div/b")
-	private WebElement linkSignOut;
+	private WebElement userNameSurname;
 
+	@FindBy(xpath = "/html/body/div[2]/div[3]/div/div/div/div[2]/div[1]/div/input")
+	private WebElement searchInput;	
+	
+	@FindBy(xpath = "/html/body/div[2]/div[3]/div/div/div/div[2]/div[4]/table/tbody/tr/td[3]")
+	private WebElement emailUserFound;	
+	
+	@FindBy(xpath = "/html/body/div[2]/div[3]/div/div/div/div[2]/div[4]/table/tbody/tr/td[9]/div/a[3]/i")
+	private WebElement deleteButton;	
+	
+	@FindBy(xpath = "/html/body/div[2]/div[3]/div/div/div/div[2]/div[4]/table/tbody/tr/td[9]/div/div[2]/div/div/div[3]/button[2]")
+	private WebElement confirmButton;	
+	
+	@FindBy(xpath = "/html/body/div[2]/div[3]/div/div/div/div[2]/div[4]/table/tbody/tr/td")
+	private WebElement dataNotFound;	
+	
+	
 	public AdminMainPage(WebDriver driver) {
 		super(driver, PageTitle.ADMIN_MAIN_PAGE, PageUrl.ADMIN_MAIN_PAGE);
 	}
 
 	public /*Boolean*/ String isLoggedIn() {
 //		return linkSignOut.isDisplayed();	
-		return linkSignOut.getText();		
+		return userNameSurname.getText();		
 	}
-
 	public AdminMainPage loginAs(UserData admin) {
 		// TODO Auto-generated method stub
 		return null;
 	}	
+
+	//Set email in searchInput
+	public AdminMainPage searchAs(UserData email) {
+		getForm().set(searchInput, email.getEmail());
+		return BasePage.create(driver, AdminMainPage.class);
+	}	
+    //Check the user is found
+	public Boolean isUserFound() {
+		return (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return emailUserFound.getText().contains("test1@test1.test1");
+            }
+        });	
+	}
+	
+	//Delete user
+	public AdminMainPage delete() {
+		deleteButton.click();
+		confirmButton.click();
+		return BasePage.create(driver, AdminMainPage.class);
+	}	
+    //Check the user is NOT found (is deleted)
+	public Boolean isDeletedUserFound() {
+		return (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return dataNotFound.getText().contains("Данные не найдены.");
+            }
+        });	
+	}	
+	
+	
+	
 }
