93ff0ed282d6752f0ce71cbd69ad134b6e2be967
Aleksey <aleksey.shevchuk@attractgroup.com>
2017-03-14 11:56:17 +0200

    This class was changed during created the 'AdminPanelAuthorizeTest' test

--- src/main/java/com/swat/data/UserData.java
+++ src/main/java/com/swat/data/UserData.java
@@ -6,24 +6,32 @@ public class UserData {
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
 
 	private String password;
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	private String email;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}	
 
 	public UserData(String name, String password) {
 		this.name = name;
 		this.password = password;
 	}
+	public UserData(String email) {
+		this.email = email;
+	}	
 
 }
