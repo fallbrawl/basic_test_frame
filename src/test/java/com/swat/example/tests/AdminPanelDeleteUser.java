fca19d96c45c2e1ab5a1d917c385b5f96d9a402c
Aleksey <aleksey.shevchuk@attractgroup.com>
2017-03-14 12:03:46 +0200

    This class was created during created the 'AdminPanelAuthorizeTest' test

---
+++ src/test/java/com/swat/example/tests/AdminPanelDeleteUser.java
@@ -0,0 +1,23 @@
+package com.swat.example.tests;
+
+import org.testng.annotations.Test;
+
+import com.swat.BasePage;
+import com.swat.data.UserData;
+import com.swat.pages.AdminMainPage;
+
+public class AdminPanelDeleteUser extends BaseTestCase {
+
+//  private UserData email = new UserData("test1@test1.test1");
+    @Test
+    public void adminPanelDeleteUser(){
+    	AdminMainPage adminMainPage = BasePage.create(getDriver(),AdminMainPage.class);
+//      adminMainPage.open();       
+
+    	adminMainPage.isUserFound();
+        adminMainPage.delete();       
+        
+		adminMainPage.isDeletedUserFound();		
+    }	
+	
+}
