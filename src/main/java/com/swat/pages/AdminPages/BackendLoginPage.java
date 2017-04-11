a3a728dfc85eb024cdc3077d2d330526d8cbb215
Aleksey <aleksey.shevchuk@attractgroup.com>
2017-04-11 17:08:27 +0300

    This classes was changed during created the TestCase #2

--- src/main/java/com/swat/pages/AdminPages/BackendLoginPage.java
+++ src/main/java/com/swat/pages/AdminPages/BackendLoginPage.java
@@ -12,24 +12,23 @@ import org.openqa.selenium.support.FindBy;
 /**
  * Created by Alex on 10.03.17.
  */
-public class BackendLoginPage extends AdminFrame {
-	
-	@FindBy(id = "admin1")
-	private WebElement email;
-	@FindBy(id = "admin2")
-	private WebElement password;	
+public class BackendLoginPage extends BasePage {
 
-	
+	@FindBy(id = "admin1")
+	private WebElement fieldEmail;
+	@FindBy(id = "admin2")
+	private WebElement fieldPassword;
+
+
 	public BackendLoginPage(WebDriver driver) {
 		super(driver, PageTitle.BACKEND_LOGIN_PAGE, PageUrl.BACKEND_LOGIN_PAGE);
-   	}
-	
+	}
+
 	//Set email and password in textbox and click on button
 
 	public AdminUsersPage loginAs(UserData admin) {
-		getForm().set(email, admin.getEmail()).set(password, admin.getPassword()).submit();
+		getForm().set(fieldEmail, admin.getEmail()).set(fieldPassword, admin.getPassword()).submit();
 		return BasePage.create(driver, AdminUsersPage.class);
 	}
-	
-}
 
+}
--- src/main/java/com/swat/pages/AdminPages/Settings/AdminUsersPage.java
+++ src/main/java/com/swat/pages/AdminPages/Settings/AdminUsersPage.java
@@ -10,6 +10,10 @@ import com.swat.pages.AdminPages.AdminFrame;
 import com.swat.staticdata.PageTitle;
 import com.swat.staticdata.PageUrl;
 
+/**
+ * Created by Alex on 13.03.17.
+ */
+
 public class AdminUsersPage extends AdminFrame {
 
 	@FindBy(id = "admin5")
@@ -17,67 +21,63 @@ public class AdminUsersPage extends AdminFrame {
 
 	@FindBy(id = "admin6")
 	private WebElement searchInput;
-	
-//	@FindBy(xpath = "/html/body/div[2]/div[3]/div/div/div/div[2]/div[4]/table/tbody/tr/td[3]")
-	@FindBy(xpath = "//table/tbody/tr/td[3]")	
-	private WebElement emailUserFound;	
-	
-//	@FindBy(xpath = "/html/body/div[2]/div[3]/div/div/div/div[2]/div[4]/table/tbody/tr/td[9]/div/a[3]/i")
-	@FindBy(xpath = "//table/tbody/tr/td[9]/div/a[3]/i")
-	private WebElement deleteButton;	
-	
-//	@FindBy(xpath = "/html/body/div[2]/div[3]/div/div/div/div[2]/div[4]/table/tbody/tr/td[9]/div/div[2]/div/div/div[3]/button[2]")
-	@FindBy(xpath = "//table/tbody/tr/td[9]/div/div[2]/div/div/div[3]/button[2]")
-	private WebElement confirmButton;	
 
-//	@FindBy(xpath = "/html/body/div[2]/div[3]/div/div/div/div[2]/div[4]/table/tbody/tr/td")
-	@FindBy(xpath = "//table/tbody/tr/td")	
-	private WebElement dataNotFound;	
-	
-	
+	@FindBy(xpath = "//table/tbody/tr/td[3]")
+	private WebElement emailUserFound;
+
+	@FindBy(xpath = "//table/tbody[1]/tr/td[10]/div/div/a[3]/i")
+	private WebElement deleteButton;
+
+	@FindBy(xpath = "//table/tbody/tr/td[10]/div/div/div[2]/div/div/div[3]/button[2]")
+	private WebElement confirmButton;
+
+	@FindBy(xpath = "//table/tbody/tr/td")
+	private WebElement dataNotFound;
+
+
 	public AdminUsersPage(WebDriver driver) {
-		super(driver, PageTitle.ADMIN_MAIN_PAGE, PageUrl.ADMIN_MAIN_PAGE);
+		super(driver);
 	}
 
 	public /*Boolean*/ String isLoggedIn() {
-		return userNameSurname.getText();		
+		return userNameSurname.getText();
 	}
 	public AdminUsersPage loginAs(UserData admin) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 
 	//Set email in searchInput
 	public AdminUsersPage searchAs(UserData email) throws InterruptedException  {
 		getForm().set(searchInput, email.getEmail());
-        Thread.sleep(500);
+		Thread.sleep(1000);
 		return BasePage.create(driver, AdminUsersPage.class);
-	}	
-    //Check the user is found
+	}
+	//Check the user is found
 /*	public Boolean isUserFound() {
 		return (new WebDriverWait(driver, 20)).until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver d) {
                 return emailUserFound.getText().contains("test1@test1.test1");
             }
-        });	
+        });
 	}*/
-    public String isUserFound() {
-        return emailUserFound.getText();
-    }	
-	
+	public String isUserFound() {
+		return emailUserFound.getText();
+	}
+
 	//Delete user
 	public AdminUsersPage delete() throws InterruptedException {
 		deleteButton.click();
 		confirmButton.click();
-        Thread.sleep(500);		
+		Thread.sleep(500);
 		return BasePage.create(driver, AdminUsersPage.class);
-	}	
+	}
 
 	//Check the user is NOT found (is deleted)
-    public String isDeletedUserFound() {
-        return dataNotFound.getText();
-    }	
-	
-	
+	public String isDeletedUserFound() {
+		return dataNotFound.getText();
+	}
+
+
 }
