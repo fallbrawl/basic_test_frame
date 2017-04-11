5cb633636f8da2a2fdf06d41d9a0f09d4ef43a78
Aleksey <aleksey.shevchuk@attractgroup.com>
2017-04-11 16:22:52 +0300

    This class was changed during created the TestCase #2

--- src/main/java/com/swat/pages/AdminPages/AdminFrame.java
+++ src/main/java/com/swat/pages/AdminPages/AdminFrame.java
@@ -5,23 +5,52 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import com.swat.BasePage;
+import com.swat.pages.AdminPages.Restaurants.AdminListPage;
 import com.swat.staticdata.PageTitle;
 import com.swat.staticdata.PageUrl;
 
-public class AdminFrame extends BasePage {
-	
-	@FindBy(id = "admin13")
-	private WebElement btnAdminMenuLogout;		
+/**
+ * Created by Alex on 17.03.17.
+ */
 
-	public AdminFrame(WebDriver driver, PageTitle title, PageUrl pageUrl) {
-		super(driver, title, pageUrl);
+public class AdminFrame extends BasePage {
+
+	@FindBy(id = "admin13")
+	private WebElement btnAdminMenuLogout;
+
+	@FindBy(id = "admin14")
+	private WebElement btnAdminMenuRestaurants;
+
+	@FindBy(id = "admin15")
+	private WebElement btnAdminMenuDictionaries;
+
+	@FindBy(id = "admin16")
+	private WebElement btnAdminMenuSettings;
+
+	@FindBy(id = "admin17")
+	private WebElement btnAdminMenuGeolocation;
+
+	@FindBy(id = "admin28")
+	private WebElement btnAdminMenuDropdownList;
+
+
+	public AdminFrame(WebDriver driver) {
+		super(driver, PageTitle.DYNAMIC_PAGE, PageUrl.DYNAMIC_PAGE);
 	}
 
-    public void adminMenuLogout() throws InterruptedException {
-        btnAdminMenuLogout.click();
+	public void adminMenuLogout() throws InterruptedException {
+		btnAdminMenuLogout.click();
+	}
 
-    }
+	public void adminMenuRestaurants() {
+		btnAdminMenuRestaurants.click();
+	}
 
-	
-	
+	public AdminListPage adminMenuDropdownList() {
+		btnAdminMenuDropdownList.click();
+		return BasePage.create(driver, AdminListPage.class);
+	}
+
+
+
 }
